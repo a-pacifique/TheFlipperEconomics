3e68e9dd51817a6fd87e246b7781e599e515d30a
a-pacifique <132818093+a-pacifique@users.noreply.github.com>
2024-03-17 22:58:52 +0200

    Update settings.py

--- theflippereconomics/settings.py
+++ theflippereconomics/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-q4up42oko9hwfb2mau8!&@g9oshldl#-3oac!#43)061!q^4nt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['13.53.140.194', 'theflippereconomics.online',]
+ALLOWED_HOSTS = ['51.20.121.204', 'theflippereconomics.online',]
 
 
 # Application definition
