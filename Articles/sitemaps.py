98157736edc78b16c66c824b65a7708e1556a1d1
a-pacifique <132818093+a-pacifique@users.noreply.github.com>
2024-04-02 23:21:10 +0200

    Update sitemaps.py

--- Articles/sitemaps.py
+++ Articles/sitemaps.py
@@ -13,16 +13,15 @@ class StaticViewSitemap(sitemaps.Sitemap):
     def location(self, item):
         return reverse(item)
 
-class ArticleSitemap(sitemaps.Sitemap):
+class ArticleSitemap(Sitemap):
     changefreq = 'weekly'
     priority = 0.9
 
     def items(self):
-        return Article.objects.filter(published=True).order_by('-date_written')[:3]
+        return Article.objects.filter(published=True)
 
-    def location(self, obj):
-        return reverse('view_article', args=[obj.slug])
-
+    def lastmod(self, obj):
+        return obj.date_written.strftime('%Y-%m-%d')
 
 
 
