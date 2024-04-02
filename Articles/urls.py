127afc578dd4ffcaaa1a3fa031a03bf3e88edb07
a-pacifique <132818093+a-pacifique@users.noreply.github.com>
2024-04-02 11:57:06 +0200

    Update urls.py

--- Articles/urls.py
+++ Articles/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import StaticViewSitemap, ArticleSitemap
 
-
+sitemaps = {
+    'static': StaticViewSitemap,
+    'articles': ArticleSitemap,
+}
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -14,4 +19,6 @@ urlpatterns = [
     path('about-us/', views.about, name='about'),
     path('contact-us/', views.contact, name='contact'),
     path('privacy-policy/', views.privacy, name='privacy'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('ads.txt', views.ads_txt, name='ads_txt'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
